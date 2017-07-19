a57c523e1516d94e3f7d888bb33fb37fcf302df0
Sophia Bok <sophiatca@gmail.com>
2017-07-19 14:35:01 -0400

    initial setup

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 var express = require('express');
 var app     = express();
 
-
+app.use(express.static(__dirname));
 app.listen(8081, function () {
   console.log('Server listening on port', 8081);
 });
